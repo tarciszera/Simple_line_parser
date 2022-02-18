5bd374d287cb7fda749751d899eab58a9038595b
Tarcis Aurelio Becher <37295838+tarciszera@users.noreply.github.com>
2022-02-18 14:50:12 -0300

    Fixed some bugs, add build script

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -o line_parser parse_lines.cpp -Wall -O3
--- parse_lines.cpp
+++ parse_lines.cpp
@@ -1,25 +1,29 @@
 #include <cstdlib>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <regex>
 #include <sstream>
 #include <string>
-#include <regex>
 
-#define VERSION 1.0
+#define VERSION 1.1
 
+/// Help message
 void usage(void) {
-  std::cout
-    << "Log parser, version " << VERSION << "\n"
-    << "Usage: "<< "./logparser src_file dst_file pattern\n";
+  std::cout << "Line parser, version " << VERSION << "\n"
+            << "Usage: "
+            << "./line_parser src_file dst_file pattern\n";
 }
 
-int main(int argc, char** argv) {
+/// Main program
+int main(int argc, char **argv) {
 
-  if (argc < 4) {
-    std::cerr << "Missing args\n";
+  /// Check for args quantity
+  if (argc != 4) {
+    std::cerr << "Wrong arguments number\n";
     usage();
     return 1;
   }
+
   /// Getting args
   std::string input_file(argv[1]), output_file(argv[2]);
   std::string pattern(argv[3]);
@@ -30,6 +34,11 @@ int main(int argc, char** argv) {
 
   /// Open file and write it to a buffer and then closing it
   fs.open(input_file, std::fstream::in);
+  if (!fs.good()) {
+    std::cout << "src_file not found\n";
+    return 1;
+  }
+
   file_ss << fs.rdbuf();
   std::string file_buffer(file_ss.str());
   fs.close();
@@ -38,27 +47,46 @@ int main(int argc, char** argv) {
   std::string reg_pattern = "[^\\s].*" + pattern + ".*\\s";
   std::regex regex_parse_line(reg_pattern);
 
-  /// Opening output file
-  fs.open(output_file, std::fstream::out | std::fstream::app);
-
   /// Getting matchs from buffer
   std::cout << "Searching for matchs on file '" << input_file << "'...\n";
   std::sregex_iterator current_match(file_buffer.begin(), file_buffer.end(),
                                      regex_parse_line);
   std::sregex_iterator last_match;
-  std::cout << "Found " << std::distance(current_match, last_match)
-    << " lines containing " << "'" << pattern << "'\n";
+
+  /// Check if there's no matchs
+  int lines_match = std::distance(current_match, last_match);
+  if (lines_match <= 0) {
+    std::cout << "Pattern not found\n";
+    return 0;
+  }
+
+  std::cout << "Found " << lines_match << " lines containing "
+            << "'" << pattern << "'\n";
 
   /// Saving matchs on file
   std::cout << "Saving matchs on file '" << output_file << "'...\n";
-  while (current_match != last_match) {
-    std::smatch match = *current_match;
-    fs << match.str();
-    current_match++;
+  std::string previous_match("");
+
+  /// Opening output file
+  fs.open(output_file, std::fstream::out | std::fstream::app);
+  if (!fs.good()) {
+    std::cout << "Coudn't open dst_file\n";
+    return 1;
   }
+
+  /// Save matchs on file
+  do {
+    std::smatch match = *current_match;
+    if (previous_match.compare(match.str())) {
+      fs << match.str();
+      previous_match = match.str();
+    }
+  } while (++current_match != last_match);
+
   std::cout << "Done!\n";
 
   /// Close and save output file
   fs.close();
+
   return 0;
 }
